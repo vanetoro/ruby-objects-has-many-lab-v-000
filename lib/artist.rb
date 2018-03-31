ebb699b593ba9fd1178444b0470201a070d1cd2a
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 22:30:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
       attr_accessor :name, :songs
-      
+      @@all = []
   def initialize(name)
     @name = name
     @songs = []
@@ -11,12 +11,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@all << song
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@all << song
   end
   
   def self.song_count
