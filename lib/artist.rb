cfab45309edcd4feda4b50cff279bfa78599589a
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 22:16:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,13 +11,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    binding.pry
   end
   
   def add_song_by_name(song)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    # binding.pry
+    
   end
   
 end
