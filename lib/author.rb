068b1f2021d827bf977f219ee398935e5893ba0a
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 22:53:49 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,8 +10,8 @@ class Author
     post.author = self
   end
   
-  def add_post_by_title(name)
-    name = Post.new(name)
+  def add_post_by_title(post)
+    post = Post.new(post)
     post.author = self
     
   end
